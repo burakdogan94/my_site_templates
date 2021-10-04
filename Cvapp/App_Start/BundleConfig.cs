5f0b740eaf250f85a86e65e6784f92ad901f8806
burakdogan94 <68888825+burakdogan94@users.noreply.github.com>
2021-10-04 15:12:19 +0300

    My Cv site added
    my cv website

---
+++ Cvapp/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Cvapp
+{
+    public class BundleConfig
+    {
+        // Paketleme hakkında daha fazla bilgi için lütfen https://go.microsoft.com/fwlink/?LinkId=301862 adresini ziyaret edin
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            
+        }
+    }
+}
---
+++ Cvapp/Controllers/CvController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cvapp.Controllers
+{
+    public class CvController : Controller
+    {
+        public ViewResult Anasayfa()
+        {
+            return View();
+        }
+        public ViewResult egitim()
+        {
+            return View();
+        }
+        public ViewResult egitimseviyesi()
+        {
+            return View();
+        }
+        public ViewResult yabancidil()
+        {
+            return View();
+        }
+        public ViewResult projeler()
+        {
+            return View();
+        }
+        public ViewResult iletisim()
+        {
+            return View();
+        }
+        public ViewResult hobiler()
+        {
+            return View();
+        }
+
+
+    }
+}
